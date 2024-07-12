f0eb3cf30c8cb805092a7ec236320360adcc3999
Matt Shoemaker <mattshoe81@gmail.com>
2024-07-12 15:39:56 -0400

    better bash

--- ci/zip.sh
+++ ci/zip.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Define the array of project names
+projects=(
+  "AutoRepo.Runtime"
+  "AutoRepo.Processor"
+  "AutoRepo.Annotations"
+)
+
 OUTPUT_DIR="./build/distributions/"
 echo "$PWD"
 
@@ -7,19 +14,11 @@ echo "$PWD"
 
 ./gradlew test --no-daemon
 
-rm -rf ~/.m2
-./gradlew :AutoRepo.Runtime:generateZip --no-daemon
-mkdir -p $OUTPUT_DIR
-cp ./AutoRepo.Runtime/build/distributions/* $OUTPUT_DIR
-
-rm -rf ~/.m2
-./gradlew :AutoRepo.Processor:generateZip --no-daemon
-mkdir -p $OUTPUT_DIR
-cp ./AutoRepo.Processor/build/distributions/* $OUTPUT_DIR
-
-rm -rf ~/.m2
-./gradlew :AutoRepo.Annotations:generateZip --no-daemon
-mkdir -p $OUTPUT_DIR
-cp ./AutoRepo.Annotations/build/distributions/* $OUTPUT_DIR
+for project in "${projects[@]}"; do
+    rm -rf ~/.m2
+    ./gradlew :"$project":generateZip --no-daemon
+    mkdir -p $OUTPUT_DIR
+    cp ./"$project"/build/distributions/* $OUTPUT_DIR
+done
 
 
