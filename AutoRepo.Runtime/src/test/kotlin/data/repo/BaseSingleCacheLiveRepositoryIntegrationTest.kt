810b96413bb20f581dd81a341ada00c5a4fabf8b
Matt Shoemaker <mattshoe81@gmail.com>
2024-07-18 18:43:43 -0400

    unit tests

--- AutoRepo.Runtime/src/test/kotlin/data/repo/BaseSingleCacheLiveRepositoryIntegrationTest.kt
+++ AutoRepo.Runtime/src/test/kotlin/data/repo/BaseSingleCacheLiveRepositoryIntegrationTest.kt
@@ -1,10 +1,12 @@
 package data.repo
 
 import app.cash.turbine.test
+import app.cash.turbine.turbineScope
 import com.google.common.truth.Truth
-import io.github.mattshoe.shoebox.data.source.impl.MemoryCachedDataSource
-import io.github.mattshoe.shoebox.data.DataResult.*
-import io.mockk.*
+import io.github.mattshoe.shoebox.data.DataResult
+import io.github.mattshoe.shoebox.data.DataResult.Error
+import io.github.mattshoe.shoebox.data.DataResult.Success
+import io.mockk.clearAllMocks
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
@@ -49,28 +51,22 @@ class BaseSingleCacheLiveRepositoryIntegrationTest {
     }
 
     @Test
-    fun `WHEN initialize is invoked multiple times sequentially THEN only the first invocation is honored`() = runTest(unconfinedTestDispatcher) {
+    fun `WHEN initialize is invoked multiple times sequentially THEN only the first invocation is executed and other dropped`() = runTest(unconfinedTestDispatcher) {
         val subject = makeSubject()
-        val queue = ArrayDeque(listOf("first", "second", "third"))
         subject.data.test {
-            val expectedValue = "first"
-            subject.operation = {
-                queue.removeFirst()
-            }
-
             subject.fetch(42)
             advanceUntilIdle()
-            subject.fetch(42)
+            subject.fetch(43)
             advanceUntilIdle()
-            subject.fetch(42)
+            subject.fetch(44)
             advanceUntilIdle()
 
-            Truth.assertThat(awaitItem()).isEqualTo(Success(expectedValue))
+            Truth.assertThat(awaitItem()).isEqualTo(Success("42"))
         }
     }
 
     @Test
-    fun `WHEN initialize is invoked multiple times concurrently THEN only one operation is made`() = runTest(standardTestDispatcher) {
+    fun `WHEN initialize is invoked multiple times concurrently THEN only one operation is executed`() = runTest(standardTestDispatcher) {
         val subject = makeSubject()
 
         subject.data.test {
@@ -146,9 +142,69 @@ class BaseSingleCacheLiveRepositoryIntegrationTest {
         }
     }
 
+    @Test
+    fun `WHEN multiple listeners THEN all receive updates`() = runTest {
+        val subject = makeSubject()
+        turbineScope {
+            val turbine1 = subject.data.testIn(backgroundScope)
+            val turbine2 = subject.data.testIn(backgroundScope)
+            val turbine3 = subject.data.testIn(backgroundScope)
+
+            subject.fetch(42)
+
+            Truth.assertThat(turbine1.awaitItem()).isEqualTo(Success("42"))
+            Truth.assertThat(turbine2.awaitItem()).isEqualTo(Success("42"))
+            Truth.assertThat(turbine3.awaitItem()).isEqualTo(Success("42"))
+
+            subject.fetch(96, forceRefresh = true)
+
+            Truth.assertThat(turbine1.awaitItem()).isEqualTo(Success("96"))
+            Truth.assertThat(turbine2.awaitItem()).isEqualTo(Success("96"))
+            Truth.assertThat(turbine3.awaitItem()).isEqualTo(Success("96"))
+        }
+    }
+
+    @Test
+    fun `WHEN subscribing after a previous emission THEN most recent value is replayed`() = runTest {
+        val subject = makeSubject()
+        turbineScope {
+            val turbine1 = subject.data.testIn(backgroundScope)
+
+            subject.fetch(42)
+            Truth.assertThat(turbine1.awaitItem()).isEqualTo(Success("42"))
+            subject.fetch(96, forceRefresh = true)
+            Truth.assertThat(turbine1.awaitItem()).isEqualTo(Success("96"))
+
+            subject.data.test {
+                Truth.assertThat(awaitItem()).isEqualTo(Success("96"))
+            }
+        }
+    }
+
     @Test(expected = IllegalStateException::class)
-    fun `WHEN refresh is invoked before initialize THEN exception is thrown`() = runTest {
+    fun `WHEN refresh is invoked before fetch THEN exception is thrown`() = runTest {
         makeSubject().refresh()
+    }
+
+    @Test
+    fun `WHEN invalidate is invoked before fetch THEN Invalidated emission occurs`() = runTest {
+        val subject = makeSubject()
+        subject.invalidate()
+        subject.data.test {
+            Truth.assertThat(awaitItem() is DataResult.Invalidated).isTrue()
+        }
+    }
+
+    @Test
+    fun `WHEN invalidate is invoked after fetch THEN Invalidated emission occurs`() = runTest {
+        val subject = makeSubject()
+        subject.data.test {
+            subject.fetch(42)
+            Truth.assertThat(awaitItem()).isEqualTo(Success("42"))
+            
+            subject.invalidate()
+            Truth.assertThat(awaitItem() is DataResult.Invalidated).isTrue()
+        }
     }
 
 
