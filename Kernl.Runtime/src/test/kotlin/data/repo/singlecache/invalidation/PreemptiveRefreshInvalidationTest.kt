c71f1e718ea8a5ac61399fa96f7644c09d544dae
Matt Shoemaker <mattshoe81@gmail.com>
2024-08-12 09:56:48 -0400

    Fixed slow test

--- Kernl.Runtime/src/test/kotlin/data/repo/singlecache/invalidation/PreemptiveRefreshInvalidationTest.kt
+++ Kernl.Runtime/src/test/kotlin/data/repo/singlecache/invalidation/PreemptiveRefreshInvalidationTest.kt
@@ -13,6 +13,9 @@ import org.mattshoe.shoebox.kernl.KernlPolicyDefaults
 import org.mattshoe.shoebox.kernl.runtime.DataResult
 import org.mattshoe.shoebox.kernl.runtime.cache.util.Stopwatch
 import org.mattshoe.shoebox.kernl.runtime.session.KernlResourceManager
+import util.measureTime
+import util.runKernlTest
+import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.measureTime
 
@@ -42,8 +45,9 @@ class PreemptiveRefreshInvalidationTest: InvalidationStrategyTest() {
         )
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `WHEN timeToLive approaches expiry THEN data is refreshed with the appropriate lead time`() = runBlocking {
+    fun `WHEN timeToLive approaches expiry THEN data is refreshed with the appropriate lead time`() = runKernlTest {
         val subject = makeSubject(
             invalidationStrategy = InvalidationStrategy.PreemptiveRefresh(
                 timeToLive = 1000.milliseconds,
@@ -53,29 +57,22 @@ class PreemptiveRefreshInvalidationTest: InvalidationStrategyTest() {
         )
 
         subject.data.test {
-            println("Fetching first round of data")
             subject.fetch(42)
-            println("awaiting first successful response")
             Truth.assertThat(awaitItem() is DataResult.Success).isTrue()
-            var elapsedTime = measureTime {
-                println("awaiting preemptive response")
-                Truth.assertThat(awaitItem() is DataResult.Success).isTrue()
-                println("preemptive response received")
-            }
-            Truth.assertThat(elapsedTime).isGreaterThan(700.milliseconds)
-            Truth.assertThat(elapsedTime).isLessThan(800.milliseconds)
+            Truth.assertThat(
+                measureTime {
+                    Truth.assertThat(awaitItem() is DataResult.Success).isTrue()
+                }
+            ).isEqualTo(700.milliseconds)
 
             // On manual invalidation, we should kick a refresh immediately upon invalidation event
-            delay(300)
-            println("delayed 300, expecting no events")
-            expectNoEvents()
-            println("manually invalidating")
-            subject.invalidate()
-            println("manually invalidated")
-            println("awaiting Invalidation emission")
-            Truth.assertThat(awaitItem() is DataResult.Invalidated).isTrue()
-            println("awaiting final preemptive emission")
-            Truth.assertThat(awaitItem() is DataResult.Success).isTrue()
+            Truth.assertThat(
+                measureTime {
+                    subject.invalidate()
+                    Truth.assertThat(awaitItem() is DataResult.Invalidated).isTrue()
+                    Truth.assertThat(awaitItem() is DataResult.Success).isTrue()
+                }
+            ).isEqualTo(Duration.ZERO)
 
             cancelAndIgnoreRemainingEvents()
         }
