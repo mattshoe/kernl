6b5d293543fb9b45490e097a2c82d7d2d7484888
Matt Shoemaker <mattshoe81@gmail.com>
2024-08-11 20:26:38 -0400

    more docs

--- Kernl.Common/src/main/kotlin/org/mattshoe/shoebox/kernl/DefaultKernlPolicy.kt
+++ Kernl.Common/src/main/kotlin/org/mattshoe/shoebox/kernl/DefaultKernlPolicy.kt
@@ -3,5 +3,16 @@ package org.mattshoe.shoebox.kernl
 import kotlinx.coroutines.flow.Flow
 import kotlin.time.Duration
 
-
-object DefaultKernlPolicy: KernlPolicy by KernlPolicyDefaults.copy()
+/**
+ * The default implementation of the `KernlPolicy` interface with predefined settings.
+ *
+ * `DefaultKernlPolicy` provides standard configurations for managing the lifecycle and behavior of cached data in a
+ * Kernl instance.
+ *
+ * ### The default values used are:
+ * - **retryStrategy**: `null` - No retry strategy is applied by default.
+ * - **events**: `kernl { globalEventStream() }` - Uses the global event stream.
+ * - **cacheStrategy**: [CacheStrategy.NetworkFirst] - Prioritizes fetching data from the network before falling back to the cache.
+ * - **invalidationStrategy**: [TakeNoAction(timeToLive = Duration.INFINITE)][InvalidationStrategy.TakeNoAction]` - No action is taken upon cache invalidation, with an infinite time-to-live.
+ */
+object DefaultKernlPolicy : KernlPolicy by KernlPolicyDefaults.copy()
