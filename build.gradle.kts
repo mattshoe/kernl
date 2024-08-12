5bde3da5bef49541bf51e7805565dfe942dd3cab
Matt Shoemaker <mattshoe81@gmail.com>
2024-08-11 21:59:22 -0400

    plz work

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,8 +72,9 @@ subprojects {
                                 url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
 
                                 credentials {
-                                    username = System.getenv("OSSRH_USERNAME")
-                                    password = System.getenv("OSSRH_PASSWORD")
+
+                                    username = providers.environmentVariable("OSSRH_USERNAME").get()
+                                    password = providers.environmentVariable("OSSRH_PASSWORD").get()
                                 }
                             }
                         }
